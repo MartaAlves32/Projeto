5ea04ab7c65ae87eea72c4b0cef36d6c1e152ac5
Marta Alves <martaalves32@yahoo.com.br>
2024-12-28 16:56:05 -0300

    exercicio 1 gulp final

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,16 @@ const uglify = require('gulp-uglify')
 const image = require ('gulp-image')
 
 
-function tarefasCSS(cb){
+function tarefasCSS(){
 
-   return gulp.src('./vendor/**/*.css')
+   return gulp.src([
+        './node_modules/bootstrap/dist/css/bootstrap.css',
+        './vendor/owl/css/owl.css',
+        './node_modules/@fortawesome/fontawesome-free/css/fontawesome.css',
+        './vendor/jquery-ui/jquery-ui.css'
+   ])
          
-      .pipe(concat('libs.css'))
+      .pipe(concat('style.css'))
       .pipe(rename({suffix: '.min'}))
       .pipe(gulp.dest('./dist'))      
 }
@@ -23,7 +28,7 @@ function tarefasJS(){
 }
 
 function tarefasImagem(){
-        return gulp.src("./src/**/*")
+        return gulp.src("./src/images/**/*")
                 .pipe(image({
                         pngquant: true,
                         optipng: false,
